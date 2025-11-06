38786ee96f2b32ccc729230ed4f8fbc2e14e9df2
Zach Handley <zachhandley@gmail.com>
2025-11-06 10:05:10 -0800

    v0.1.7PluginServiceRegistrator

---
+++ Jellyfin.Plugin.Template/PluginServiceRegistrator.cs
@@ -0,0 +1,20 @@
+using Jellyfin.Plugin.Template.Authentication;
+using MediaBrowser.Controller;
+using MediaBrowser.Controller.Authentication;
+using MediaBrowser.Controller.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jellyfin.Plugin.Template;
+
+/// <summary>
+/// Registers plugin services with Jellyfin's dependency injection container.
+/// </summary>
+public class PluginServiceRegistrator : IPluginServiceRegistrator
+{
+    /// <inheritdoc />
+    public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
+    {
+        // Register the Appwrite authentication provider
+        serviceCollection.AddSingleton<IAuthenticationProvider, AppwriteAuthenticationProvider>();
+    }
+}
